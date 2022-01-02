936f19d116e50b87611064b97bebb7f4660ae4e1
kotobukihuku <86099142+kotobukihuku@users.noreply.github.com>
2022-01-02 22:31:04 +0900

    修正しました

--- js/main.js
+++ js/main.js
@@ -1,24 +1,11 @@
 'use strict'
 
-const que = window.prompt('日本の首都は？');
-const ans = true;
+const tst = window.prompt('日本の首都は？');
 
-while(ans) {
+while(true) {
   if(que === '東京') {
     window.alert('正解です！');
   } else {
     window.alert('不正解です！');
   }
 }
-
-// const que = window.prompt('日本の首都は？');
-// const ans = true;
-
-// while(ans){
-//   if(!que == "東京") {
-//   alert("不正解です！");
-//   continue;
-//   }
-//   alert("正解です！");
-//   }
-  
