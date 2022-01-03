f1196d496afad8b10ae10725dbdd2e91ba488d27
kotobukihuku <86099142+kotobukihuku@users.noreply.github.com>
2022-01-03 21:54:08 +0900

    略語の部分を修正しました

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 'use strict'
 
-const tst = window.prompt('日本の首都は？');
+const question = window.prompt('日本の首都は？');
 
 while(true) {
-  if(que === '東京') {
+  if(question === '東京') {
     window.alert('正解です！');
   } else {
     window.alert('不正解です！');
