1d9c8f3318e66e2c6cbe11477be0ee7f35370122
kotobukihuku <86099142+kotobukihuku@users.noreply.github.com>
2022-01-09 22:22:09 +0900

    あまり前回と変わっておりませんが、念の為提出させて頂きます。

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 'use strict'
 
-const question = window.prompt('日本の首都は？');
+const question = prompt('日本の首都は？');
 
 while(true) {
   if(question === '東京') {
-    alert('正解です！');
-    continue;
+    console.log('正解です！');
   } else {
-    alert('不正解です！');
+    console.log('不正解です！');
+    continue;
   }
 }
