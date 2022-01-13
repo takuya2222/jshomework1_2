250bca6a5b7284fe6eac1dda198411c2eb014d9e
kotobukihuku <86099142+kotobukihuku@users.noreply.github.com>
2022-01-13 23:59:05 +0900

    修正致しました。

--- js/main.js
+++ js/main.js
@@ -1,25 +1,12 @@
 'use strict'
 
-const question = prompt('日本の首都は？');
 
 while(true) {
+  const question = prompt('日本の首都は？');
   if(question === '東京') {
-    console.log('正解です！');
+    alert('正解です！');
     break;
   } else {
-    console.log('不正解です！');
-    continue;
+    alert('不正解です！');
   }
 }
-
-
-// 「日本の首都は？」という質問がでる。
-// ①正解の場合
-// 「東京」と答える。
-// →「正解！」という表記が出る。
-// →OKを押すと処理が終了する。
-
-// ②不正解の場合
-// 「東京」以外の回答をする。
-// →「不正解！」という表記が出る。
-// →OKを押すと「日本の首都は？」の質問に戻る
